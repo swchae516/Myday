3cbf935271f20c797805f7ac51cd628b04aaf4bb
juyong <ht9943@naver.com>
2022-04-15 11:16:11 +0900

    [S06P31C205-44] 환경설정

--- front/src/pages/Main.jsx
+++ front/src/pages/Main.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Cards = styled.div`
+  background-color: #f9fafb;
+  height: 200px;
+  padding-bottom: 5%;
+  padding-top: 5%;
+  padding-left: 5%;
+  padding-right: 5%;
+`;
 
 function Main() {
-  return <div>메인 페이지</div>;
+  return (
+    <Cards />
+  );
 }
 
 export default Main;
