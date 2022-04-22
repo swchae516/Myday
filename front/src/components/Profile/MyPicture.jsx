824455d3b6571bf7e5ff2426bc0b16937d21becf
juyong <ht9943@naver.com>
2022-04-22 18:29:41 +0900

    [S06P31C205-102] feat: create grass

--- front/src/components/Profile/MyPicture.jsx
+++ front/src/components/Profile/MyPicture.jsx
@@ -4,6 +4,9 @@ import { EditOutlined } from '@ant-design/icons'
 import { getAxios } from '../../api'
 import ImageFileInput from '../ImageFileInput'
 import './MyPicture.css'
+import { useSelector } from 'react-redux'
+import { Button } from 'antd'
+
 // const MyPic = styled.div`
 //   margin-left: auto;
 //   margin-right: auto;
@@ -14,56 +17,68 @@ import './MyPicture.css'
 //   overflow: hidden;
 //   position: relative;
 // `
-function MyPicture({ imageUploader, form }) {
+const Submit = styled(Button)`
+  margin-top: 10px;
+`
+function MyPicture({ imageUploader, data }) {
+  const { me } = useSelector((state) => state.user)
+  const axios = getAxios()
+
   const [file2, setFile2] = useState({ fileName: null, fileURL: null })
+  const formRef = useRef()
+
   const onFileChange = (file2) => {
     setFile2({
       fileName: file2.name,
       fileURL: file2.url,
     })
   }
-  const formRef = useRef()
-
   const onSubmit = (event) => {
     event.preventDefault()
     const data = {
-      id: Date.now(), // uuid
-      fileName: file2.fileName || '',
-      fileURL: file2.fileURL || '',
+      image: file2.fileURL || '',
     }
+    axios
+      .post(`dairy/${me.userId}`, data)
+      .then((res) => {
+        console.log(res)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
     formRef.current.reset()
-    console.log(data)
   }
-  let [file, setFiles] = useState('')
-  const onLoadFile = (e) => {
-    const file = e.target.files[0]
-    setFiles(file)
-  }
-  console.log(file2)
-  const preview = () => {
-    let profile_image = null
-    const imgEl = document.querySelector('.img__box')
-    const reader = new FileReader()
-    if (file !== '') {
-      profile_image = <img className="profile_image" src={file}></img>
-    }
-    reader.onload = () => (imgEl.style.backgroundImage = `url(${reader.result})`)
-    reader.readAsDataURL(file)
-  }
+  // const formRef = useRef()
 
-  console.log(file)
+  // const onSubmit = (event) => {
+  //   event.preventDefault()
+  //   const data = {
+  //     id: Date.now(), // uuid
+  //     fileName: file2.fileName || '',
+  //     fileURL: file2.fileURL || '',
+  //   }
+  //   formRef.current.reset()
+  // }
+  // const onLoadFile = (e) => {
+  //   const file = e.target.files[0]
+  //   setFiles(file)
+  // }
+  // const preview = () => {
+  //   let profile_image = null
+  //   const imgEl = document.querySelector('.img__box')
+  //   const reader = new FileReader()
+  //   if (file !== '') {
+  //     profile_image = <img className="profile_image" src={file}></img>
+  //   }
+  //   reader.onload = () => (imgEl.style.backgroundImage = `url(${reader.result})`)
+  //   reader.readAsDataURL(file)
+  // }
+
   const imageInput = useRef()
   const onClickImageUpload = () => {
     imageInput.current.click()
   }
 
-  // const axios = getAxios()
-
-  // const getPic = async () => {
-  //   await axios.get(`/api/user/read/{userId}`, {
-  //     pic: image,
-  //   })
-  // }
   return (
     <div>
       <div className="MyPic">
@@ -72,11 +87,15 @@ function MyPicture({ imageUploader, form }) {
           imageUploader={imageUploader}
           onFileChange={onFileChange}
           file={file2}
-          form={form}
+          // form={form}
+          data={data}
           setFile={setFile2}
           className="profileImage"
         />
       </div>{' '}
+      <Submit type="primary" onClick={onSubmit}>
+        등록
+      </Submit>
       <EditOutlined onClick={onClickImageUpload}></EditOutlined>
     </div>
   )
