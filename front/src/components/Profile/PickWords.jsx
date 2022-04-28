ff5264e171196db586c85a09e479538266203155
juyong <ht9943@naver.com>
2022-04-29 01:31:38 +0900

    [S06P31C205-148] feat: create wordpick

--- front/src/components/Profile/PickWords.jsx
+++ front/src/components/Profile/PickWords.jsx
@@ -1,5 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
+import { getAxios } from '../../api'
+import { useSelector } from 'react-redux'
+import { Row, Col, Divider } from 'antd'
+const style = { background: '#0092ff', padding: '8px 0' }
 
 const Words = styled.div`
   width: 90%;
@@ -14,10 +18,43 @@ const Search = styled.input`
   float: right;
 `
 function PickWords() {
+  const { me } = useSelector((state) => state.user)
+  console.log(me)
+  const axios = getAxios()
+  const [words, setWords] = useState([])
+
+  // const request =
+  //   me !== null &&
+  //   me.userId !== null &&
+  //   axios.get('diary/myword', { params: { userId: me.userId } })
+  // console.log(body)
+  // console.log(data)
+  const getWords = async (res) => {
+    const request =
+      me !== null &&
+      me.userId !== null &&
+      (await axios.get('diary/myword', { params: { userId: me.userId } }))
+    // console.log(request, '하이')
+    console.log(res)
+  }
+  console.log(words)
   return (
     <div>
       <Words>
         내가 선택한 단어 <Search></Search>
+        <hr />
+        <Row gutter={16}>
+          {me !== null &&
+            me.dairies.map((a, i) => {
+              return (
+                <Col key={i} className="gutter-row" span={6}>
+                  {a.word}
+                </Col>
+              )
+
+              // <div key={i}>{a.word}</div>
+            })}
+        </Row>
       </Words>
     </div>
   )
