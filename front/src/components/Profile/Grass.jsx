76b4decac4b8b59eb24001d3d27015b18a9bb3ab
이현정 <h.jung0409@daum.net>
2022-05-02 13:32:51 +0900

    [S06P31C205-159] 프로필 - 달력 컴포넌트

--- front/src/components/Profile/Grass.jsx
+++ front/src/components/Profile/Grass.jsx
@@ -1,54 +1,23 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
-import { CaretLeftOutlined, CaretRightOutlined } from '@ant-design/icons'
-import Calendar from 'react-calendar'
-import 'react-calendar/dist/Calendar.css'
-import Calen from './Calen'
-const MyGrass = styled.div`
-  margin-top: 6%;
-  width: 90%;
-  height: 280px;
-  background-color: rgb(238, 167, 187);
-  text-align: left;
-  padding-left: 5%;
-  padding-top: 2%;
-`
-// const Calener = styled.div`
-//   margin-left: 25%;
-// `
-const Month = styled.p`
-  float: right;
-  padding-right: 48%;
-`
-const MyRecord = styled.div`
-  border: solid;
-  width: 15px;
-  height: 15px;
-  margin-left: 5px;
-  margin-top: 5px;
-  border-color: green;
-`
+import { Calendar, Select, Radio, Col, Row, Typography } from 'antd'
+
+function onPanelChange(value, mode) {
+  console.log(value, mode)
+}
 
 function Grass() {
-  const [value, onChange] = useState(new Date())
-  let [days, setDays] = useState([])
-
   return (
-    <div>
-      <MyGrass>
-        잔디심기
-        {/* <Month>
-          <CaretLeftOutlined /> 4월 <CaretRightOutlined />
-        </Month> */}
-        {/* <Calen>
-          <Calendar onChange={onChange} value={value} />
-        </Calen> */}
-        {/* {days.map((a, i) => {
-          return <MyRecord key={i}></MyRecord>
-        })} */}
-        <Calen></Calen>
-      </MyGrass>
-    </div>
+    <StyledCalender>
+      <Calendar fullscreen={false} onPanelChange={onPanelChange} />
+    </StyledCalender>
   )
 }
+
+const StyledCalender = styled.div`
+  width: 300px;
+  border: 1px solid #f0f0f0;
+  border-radius: 2px;
+`
+
 export default Grass
