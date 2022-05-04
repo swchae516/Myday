68158f3f17cb63c7f5f4e99b80ce5cdd5524dc1b
이현정 <h.jung0409@daum.net>
2022-05-04 10:11:13 +0900

    [S06P31C205-168] fix: cors 설정

--- front/src/api.js
+++ front/src/api.js
@@ -5,13 +5,13 @@ const Axios = axios.create({
   // baseURL: 'http://localhost:8080',
   headers: {
     'Content-type': 'application/json',
-    // 'Access-Control-Allow-Origin': '*',
-    // 'Access-Control-Allow-Credentials: 'true',
-    // 'Access-Control-Expose-Headers': 'Content-Length,Content-Range',
-    // 'Access-Control-Max-Age': '3600',
-    // 'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-    // 'Access-Control-Allow-Headers':
-    // 'Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': 'true',
+    'Access-Control-Expose-Headers': 'Content-Length,Content-Range',
+    'Access-Control-Max-Age': '3600',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    'Access-Control-Allow-Headers':
+      'Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization',
   },
 })
 
