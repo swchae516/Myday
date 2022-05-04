3cf51fc992531989667157c1330d97f85a4e5b89
JunWoo Kim <k1mjunooo@naver.com>
2022-05-04 15:17:19 +0900

    hi

--- back/src/main/java/com/example/back/config/CorsFilter.java
+++ back/src/main/java/com/example/back/config/CorsFilter.java
@@ -25,7 +25,7 @@ public class CorsFilter implements Filter {
         res.setHeader("Access-Control-Allow-Origin", "http://k6c205.p.ssafy.io");
         res.setHeader("Access-Control-Allow-Credentials", "true");
         res.setHeader("Access-Control-Allow-Methods","*");
-        res.setHeader("Access-Control-Max-Age", "3600");
+//        res.setHeader("Access-Control-Max-Age", "3600");
         res.setHeader("Access-Control-Allow-Headers",
                 "Origin, X-Requested-With, Content-Type, Accept, Authorization");
         if("OPTIONS".equalsIgnoreCase(req.getMethod())) {
