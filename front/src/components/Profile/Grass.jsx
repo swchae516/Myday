726da43ddf8850789428302b929bacb92d4ae6fa
이현정 <h.jung0409@daum.net>
2022-05-06 17:48:54 +0900

    [S06P31C205-180] feat: 글 작성 체크 구현 중

--- front/src/components/Profile/Grass.jsx
+++ front/src/components/Profile/Grass.jsx
@@ -9,15 +9,23 @@ function onPanelChange(value, mode) {
 function Grass() {
   return (
     <StyledCalender>
-      <Calendar fullscreen={false} onPanelChange={onPanelChange} />
+      <MyCalender fullscreen={false} onPanelChange={onPanelChange} />
     </StyledCalender>
   )
 }
 
 const StyledCalender = styled.div`
   width: 300px;
+  margin: 1rem auto;
   border: 1px solid #f0f0f0;
   border-radius: 2px;
+`
+const MyCalender = styled(Calendar)`
+  .ant-picker-calendar-date-value {
+    color: yellow;
+    background-color: red;
+    border-radius: 15px;
+  }
 `
 
 export default Grass
