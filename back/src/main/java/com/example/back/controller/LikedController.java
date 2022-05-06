3bc411cfc1355f4eb081dba2dc44065d3c97945d
yejin <jasmin960427@naver.com>
2022-05-06 09:54:20 +0900

    build: 젠킨스 자동배포 시험

--- back/src/main/java/com/example/back/controller/LikedController.java
+++ back/src/main/java/com/example/back/controller/LikedController.java
@@ -62,4 +62,9 @@ public class LikedController {
     public void test(){
         System.out.println("테스트야");
     }
+    @GetMapping("/test2")
+    @ApiOperation(value = "반영되는가222", notes = "반영되라222", response = String.class)
+    public void test2(){
+        System.out.println("테스트야222");
+    }
 }
