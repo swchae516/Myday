6ffd3da400b86d62969eb7bfea176cca53dd46e6
juyong <ht9943@naver.com>
2022-05-09 18:23:26 +0900

    [S06P31C205-194] feat: main page animation

--- front/src/components/Profile/MyPicture.jsx
+++ front/src/components/Profile/MyPicture.jsx
@@ -28,6 +28,7 @@ function MyPicture({ imageUploader, data }) {
   const axios = getAxios()
 
   const [file2, setFile2] = useState({ fileName: null, fileURL: null })
+  console.log('file2', file2)
   const formRef = useRef()
   const [editable, setEditable] = useState(false)
   console.log(me)
@@ -76,8 +77,10 @@ function MyPicture({ imageUploader, data }) {
         { image: image.fileURL, age: me.age, gender: me.gender },
         { params: { userId: me.userId } },
       ))
+    setFile2({ fileName: null, fileURL: null })
     dispatch(loadUserRequestAction({ userId: me.userId }))
     setEditable(!editable)
+    console.log('file2', file2)
   }
   const success = () => {
     Modal.success({
