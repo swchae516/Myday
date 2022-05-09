659ab41ae84a23ca37e8abf98c6788b60b372ea5
이현정 <h.jung0409@daum.net>
2022-05-09 18:08:54 +0900

    [S06P31C205-195] feat: 작성날짜 데이터 바인딩 구현

--- front/src/components/Profile/Grass.jsx
+++ front/src/components/Profile/Grass.jsx
@@ -1,31 +1,81 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
-// import { Calendar } from 'antd'
 import Calendar from 'react-calendar'
 import 'react-calendar/dist/Calendar.css'
 import moment from 'moment'
+import { getAxios } from '../../api'
+import { useDispatch, useSelector } from 'react-redux'
+import { loadUserRequestAction } from '../../reducers/user'
 
-function onPanelChange(value, mode) {
-  console.log(value, mode)
-}
+const axios = getAxios()
 
 function Grass() {
+  const dispatch = useDispatch()
+  const { me } = useSelector((state) => state.user)
+  const [loading, setLoading] = useState(false)
+
   const [value, onChange] = useState(new Date())
-  const [mark, setMark] = useState(['2022-05-02', '2022-05-09', '2022-05-10'])
+  const [mark, setMark] = useState([])
+  const [curYear, setCurYear] = useState(parseInt(moment(new Date()).format('YYYY')))
+  const [curMonth, setCurMonth] = useState(parseInt(moment(new Date()).format('M')))
+
+  const onActiveStartDateChange = ({ action, activeStartDate, value, view }) => {
+    console.log('Changed view to: ', moment(activeStartDate).format('YYYY-M'), view)
+    setCurYear(parseInt(moment(activeStartDate).format('YYYY')))
+    setCurMonth(parseInt(moment(activeStartDate).format('M')))
+  }
+
+  const getJandi = async () => {
+    try {
+      let res = await axios.get('user/jandi', {
+        params: {
+          month: curMonth,
+          userId: me.userId,
+          year: curYear,
+        },
+      })
+      console.log('jandi: ', res.data)
+      setMark([...res.data.jandis])
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  const loadMoreData = (userId) => {
+    if (loading) {
+      return
+    }
+    dispatch(loadUserRequestAction({ userId: me.userId }))
+    setLoading(true)
+  }
+
+  useEffect(() => {
+    if (me != null) {
+      if (me.userId != null) {
+        loadMoreData(me.userId)
+        getJandi()
+      }
+    }
+  }, [me])
+
+  useEffect(() => {
+    getJandi()
+  }, [curYear, curMonth])
 
   return (
-    // <StyledCalender>
-    //   <MyCalender fullscreen={false} onPanelChange={onPanelChange} />
-    // </StyledCalender>
     <StyledCalender>
+      {/* <h5>
+        {curYear}년 {curMonth}월<br />
+        {mark}
+      </h5> */}
       <MyCalender
+        onActiveStartDateChange={onActiveStartDateChange}
         onChange={onChange}
         formatDay={(locale, date) => moment(date).format('DD')}
         value={value}
-        // showNeighboringMonth={false} //  이전, 이후 달의 날짜는 보이지 않도록 설정
         className="mx-auto w-full text-sm border-b"
         tileContent={({ date, view }) => {
-          if (mark.find((x) => x === moment(date).format('YYYY-MM-DD'))) {
+          if (mark.find((x) => x === moment(date).format('YYYY-M-D'))) {
             return (
               <>
                 <div className="flex justify-center items-center absoluteDiv">
@@ -35,6 +85,11 @@ function Grass() {
             )
           }
         }}
+        // tileClassName={({ date, view }) => {
+        //   if (mark.find((x) => x === moment(date).format('YYYY-M-D'))) {
+        //     return 'highlight'
+        //   }
+        // }}
       />
     </StyledCalender>
   )
@@ -46,18 +101,17 @@ const StyledCalender = styled.div`
   // border: 1px solid #f0f0f0;
   // border-radius: 2px;
 `
-// const MyCalender = styled(Calendar)`
-//   .ant-picker-calendar-date-value {
-//     color: yellow;
-//     background-color: red;
-//     border-radius: 15px;
-//   }
-// `
+
 const MyCalender = styled(Calendar)`
   abbr[title],
   abbr[data-original-title] {
     text-decoration: none;
   }
+
+  .highlight {
+    background-color: rgba(255, 218, 229, 0.5);
+  }
+
   .react-calendar__tile--active {
     position: relative;
     background: #ffdae5;
@@ -78,6 +132,7 @@ const StyledDot = styled.div`
   border-radius: 50%;
   display: flex;
   margin: -20px 5px 0 6px;
+  z-index: 0;
 `
 
 export default Grass
